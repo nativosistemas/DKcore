b9066959aa029aeb1d534b8da8448c3f45b5deb7
nativosistemas <nativosistemas@outlook.com.ar>
2020-10-28 00:38:16 -0300

    add farmacia

---
+++ DKcore/Controllers/FarmaciaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DKbase.Entities;
+using DKbase.web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DKcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FarmaciaController : ControllerBase
+    {
+        [HttpGet]
+        public List<Farmacia> Get()
+        {
+            return acceso.RecuperarFarmacias();
+        }
+    }
+}
