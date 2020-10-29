c33b5b39b7a523adacad37f72c453d2ea23487bf
nativosistemas <nativosistemas@outlook.com.ar>
2020-10-29 00:38:35 -0300

    add transfer

---
+++ DKcore/Controllers/ModuloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DKbase.Entities;
+using DKbase.web;
+
+namespace DKcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModuloController : ControllerBase
+    {
+        [HttpGet]
+        public List<Modulo> Get()
+        {
+            return acceso.RecuperarTodosModulos();
+        }
+    }
+}
