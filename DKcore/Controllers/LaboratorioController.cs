f6bce299246f5bbd677f9a032be0ada8f4db1e8d
nativosistemas <nativosistemas@outlook.com.ar>
2020-11-21 00:54:58 -0300

    moduloV2

--- DKcore/Controllers/LaboratorioController.cs
+++ DKcore/Controllers/LaboratorioController.cs
@@ -2,23 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using DKbase.Entities;
 using DKbase.app;
+using DKbase.Entities;
+using DKcore.Helpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using DKcore.Helpers;
 
 namespace DKcore.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class FarmaciaController : ControllerBase
+    public class LaboratorioController : ControllerBase
     {
-        //[Authorize]
+        [Authorize]
         [HttpGet]
-        public List<Farmacia> Get(string ApNombre)
+        public List<Laboratorio> Get()
         {
-            return accesoApp.RecuperarFarmacias(ApNombre);
+            return accesoApp.GetLaboratorios();
         }
     }
 }
