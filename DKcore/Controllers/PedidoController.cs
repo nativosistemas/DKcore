d4a046be54ddaaeae705f38ae7dff5ad38e93c09
nativosistemas <nativosistemas@outlook.com.ar>
2021-01-04 00:41:35 -0300

    api pedido

---
+++ DKcore/Controllers/PedidoController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DKbase.app;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DKcore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PedidoController : Controller
+    {
+       // [Authorize]
+        [HttpPost]
+        public IActionResult Create([FromBody] DKbase.Entities.AppPedido parameter)
+        {
+            return Ok(accesoApp.AddPedido(parameter));
+       
+        }
+    }
+}
